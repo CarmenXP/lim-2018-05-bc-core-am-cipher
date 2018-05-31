34d4ab45934a024f0af817e7d96ddcfc863a21e1
NancyMontalvo21 <nancy.montalvo21@gmail.com>
2018-05-31 09:55:15 -0500

    Nueva modificacion de Index, style

--- src/cipher.js
+++
@@ -1,6 +0,0 @@
-
-window.cipher = {
-  decode()
-
-};
-
--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 
-
-var botones =document.getElementById(cambio);
-botones.addEventListener("click",function(){})
-
-
+//
+// Asignamos todas las Variables 
+// Variable que permitirá obtener el texto de entrada del elemento mensaje
+let mensajeUsuario;
+// Variable que permitirá obtener el numero de posicion del elemento number
+let numeroPosiciones;
+// Variable que permitirá ejecutar un evento al hacer click en botón encode
+let btnCifrar = document.getElementById('encode');
+// Variable que permitirá ejecutar un evento al hacer click en botón decode
+let btnDescifrar = document.getElementById('decode');
+////Creamos una variable donde se mostrara el mensaje Cifrado-Descifrado
+let mensajeCiyDe= document.getElementById('respuesta')
+// Juntamos los eventos correspondiente a los botones de Encode, Decode.
+// Boton Encode
+btnCifrar.addEventListener('click', ()=> {
+  //Creamos una variable donde se mostrara el mensaje cifrado
+  mensajeUsuario = document.getElementById('escribir').value;
+  numeroPosiciones=document.getElementById('offset').value;
+  cipher.encode(offset.value, mensajeUsuario);
+  document.getElementById('respuesta').innerHTML = btnCifrar;
+  
+})
+//Boton Decode
+btnDescifrar.addEventListener('click', function () {
+  //Creamos una variable donde se mostrara el mensaje cifrado
+  mensajeUsuario = document.getElementById('escribir').value;
+  numeroPosiciones=document.getElementById('offset').value;
+  cipher.decode(offset.value,mensajeUsuario);
+  document.getElementById('respuesta').innerHTML = btnDescifrar;
+});
